f0cdb8b9331b91910eb04f8e8ecc03007a1481bb
Fadhilah <blyattt1211@gmail.com>
2026-04-20 11:14:20 +0700

    update tambah fitur tambah berita

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -35,25 +35,26 @@ RadioGroup radioGroup;
         editpassword = findViewById(R.id.password);
         btn = findViewById(R.id.btn);
 
-        btn.setOnClickListener(View -> {
-         String nama =editnama.getText().toString().trim();
-            String email =editemail.getText().toString().trim();
-            String password =editpassword.getText().toString().trim();
+        btn.setOnClickListener(view -> { // Gunakan 'view' kecil (best practice)
+            String nama = editnama.getText().toString().trim();
+            String email = editemail.getText().toString().trim();
+            String password = editpassword.getText().toString().trim();
 
-            if(nama.isEmpty() & email.isEmpty() & password.isEmpty()){
-                Toast.makeText(this,"Wajib di isi semua ya diks",Toast.LENGTH_SHORT).show();
-            } else if (nama.equals("budak1") &&password.equals("aduhai")&&email.equals("mamang"))
-            {
-                Toast.makeText(this,"Berhasil loh ya re",Toast.LENGTH_SHORT).show();
+            // Gunakan || (OR) supaya kalau SALAH SATU kosong, langsung kena tegur
+            if (nama.isEmpty() || email.isEmpty() || password.isEmpty()) {
+                Toast.makeText(this, "Wajib di isi semua ya diks", Toast.LENGTH_SHORT).show();
+            }
+            // Gunakan && (Double AND) untuk logika yang benar
+            else if (nama.equals("budak1") && password.equals("aduhai") && email.equals("mamang")) {
+                Toast.makeText(this, "Berhasil loh ya re", Toast.LENGTH_SHORT).show();
                 Intent intent = new Intent(MainActivity.this, MainRongngawi.class);
                 startActivity(intent);
                 finish();
             }
-            else
-            {
-                Toast.makeText(this,"Gagal cik",Toast.LENGTH_SHORT).show();
+            else {
+                Toast.makeText(this, "Gagal cik, cek lagi datanya", Toast.LENGTH_SHORT).show();
             }
-
         });
+
     }
 }
--- app/src/main/java/com/example/myapplication/MainRongngawi.java
+++ app/src/main/java/com/example/myapplication/MainRongngawi.java
@@ -1,8 +1,10 @@
 package com.example.myapplication;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
@@ -11,31 +13,20 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.ArrayList;
+
 public class MainRongngawi extends AppCompatActivity {
 ListView listngawi;
+ArrayList<String>listjudul;
+ArrayAdapter<String>adapter;
 
-String [] judul={
-        "Korban Perang Dunia I (1914-1918)",
-        "Korban Perang Dunia II (1939-1945) ",
-        " Perang Punik (264–146 SM) "
-};
-String [] isi={
-        "15 hingga 22 juta kematian (militer dan sipil) dan lebih dari 20 juta orang luka-luka....",
-        "korban jiwa diperkirakan mencapai 70 hingga lebih dari 85 juta orang....",
-        "Perang Punik (264–146 SM) adalah tiga rangkaian konflik besar antara Republik Romawi dan Kartago (Punisia) yang memperebutkan dominasi di Mediterania Barat..."
-};
+Button tambahbdkbr;
+ArrayList<String>listisi;
+    ArrayList<String>listlink;
+    ArrayList<String>listvidio;
 
-String [] link={
-     "https://id.wikipedia.org/wiki/Perang_Dunia_I",
-        "https://id.wikipedia.org/wiki/Perang_Dunia_II",
-        "https://id.wikipedia.org/wiki/Perang_Punik"
-};
-String [] video={
-        "https://youtu.be/yFQrh_KK2ws?si=EoiH8XbmxRcXWiCR",
-        "https://youtu.be/fuNjaA7RUrE?si=sdUnXOYerAIbQMfh",
-         "https://youtu.be/YIA0bcC_BNk?si=hJqQf42FjE7TYROn"
-};
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -46,9 +37,26 @@ String [] video={
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        tambahbdkbr=findViewById(R.id.tambahbdkbr);
         listngawi=findViewById(R.id.listngawi);
-        ArrayAdapter<String>adapter=new ArrayAdapter<>(
-                this,android.R.layout.simple_list_item_1,judul
+
+        listjudul=new ArrayList<>();
+        listisi=new ArrayList<>();
+        listlink=new ArrayList<>();
+        listvidio=new ArrayList<>();
+
+        listjudul.add("Korban Perang Dunia I (1914-1918)");
+        listisi.add("15 hingga 22 juta kematian (militer dan sipil) dan lebih dari 20 juta orang luka-luka....");
+        listlink.add("https://id.wikipedia.org/wiki/Perang_Dunia_I");
+        listvidio.add("https://youtu.be/yFQrh_KK2ws?si=EoiH8XbmxRcXWiCR");
+
+
+        listjudul.add("Korban Perang Dunia II (1939-1945) ");
+        listisi.add("korban jiwa diperkirakan mencapai 70 hingga lebih dari 85 juta orang....");
+        listlink.add( "https://id.wikipedia.org/wiki/Perang_Dunia_II");
+        listvidio.add( "https://youtu.be/fuNjaA7RUrE?si=sdUnXOYerAIbQMfh");
+        adapter=new ArrayAdapter<>(
+                this,android.R.layout.simple_list_item_1,listjudul
         );
         listngawi.setAdapter(adapter);
         listngawi.setOnItemClickListener((parent, view, position, id) -> {
@@ -56,12 +64,35 @@ String [] video={
 
             Intent intent = new Intent(MainRongngawi.this, ngawiActivity.class);
 
-            intent.putExtra("judul", judul[position]);
-            intent.putExtra("isi", isi[position]);
-            intent.putExtra("link", link[position]);
-            intent.putExtra("video", video[position]);
+            intent.putExtra("judul", listjudul.get(position));
+            intent.putExtra("isi", listisi.get(position));
+            intent.putExtra("link", listlink.get(position));
+            intent.putExtra("video", listvidio.get(position));
 
             startActivity(intent);
         });
+        tambahbdkbr.setOnClickListener(view -> {
+            Intent intent = new Intent(MainRongngawi.this,jeskejes.class);
+            startActivityForResult(intent,1);
+       });
     }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+
+        if (requestCode == 1 && resultCode == RESULT_OK) {
+            String judul = data.getStringExtra("judul");
+            String isi = data.getStringExtra("isi");
+            String link = data.getStringExtra("link");
+            String vidio = data.getStringExtra("vidio");
+                listjudul.add(judul);
+                listisi.add(isi);
+                listlink.add(link);
+                listvidio.add(vidio);
+                adapter.notifyDataSetChanged();
+
+        }
+    }
+
 }
